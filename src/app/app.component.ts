30adef8d2f4d2480a3bf868cf22bc79e64dc83da
Brian Sithole <briansworkman@gmail.com>
2018-10-05 14:33:58 +0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpEventType } from '@angular/common/http';
+import { resolve } from 'q';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'fileUploader';
+  percent = 0;
+  fileName = '';
+  bar: string;
+  selectedFile: File = null;
+  // uri = 'http://localhost:5000/';
+
+  constructor(private http: HttpClient) { }
+
+  onFileSelected(event) {
+    this.selectedFile = <File>event.target.files[0];
+    this.fileName = this.selectedFile.name
+    console.log(event);
+  }
+
+  onUpload(event) {
+    this.bar = 'bar'
+    const uploadData = new FormData();
+    uploadData.append('myFile', this.selectedFile, this.selectedFile.name);
+    
+    return this.http.post('https://us-central1-fileuploader-787db.cloudfunctions.net/uploadFile', uploadData, {
+      reportProgress: true,
+      observe: 'events'
+    })
+    .subscribe(res => {
+      if (res.type === HttpEventType.UploadProgress) {
+        this.percent = Math.round((res.loaded / res.total) * 100);
+        console.log('Upload progress' + this.percent  + '%')
+      } else {
+        console.log(res);
+      }
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { MatButtonModule, MatInputModule, MatProgressBarModule, MatCardModule, MatToolbarModule  } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const routes: Routes = [
+  { path: 'list', component: AppComponent },
+  { path: '', redirectTo: 'list', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule, MatInputModule, MatProgressBarModule, MatCardModule, MatToolbarModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
